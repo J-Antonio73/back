e6c1f28c384045c29d07631b6597feeca34a6a3e
Antonio <esciper1@gmail.com>
2024-01-24 12:57:14 -0600

    delete cors

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const cors = require("cors");
 const rutas = require("./src/server/routes");
-const puppeteer = require("puppeteer");
 const PORT = process.env.PORT || 3001;
-
-// app.use(
-// 	cors({
-// 		origin: `${process.env.CLIENT_URL}`,
-// 		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-// 		credentials: true,
-// 	})
-// );
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -31,28 +21,6 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", rutas);
-
-// Puppeteer configuration
-(async () => {
-	try {
-		const browser = await puppeteer.launch({
-			executablePath: process.env.CHROME_BIN || null,
-			args: [
-				"--no-sandbox",
-				"--disable-setuid-sandbox",
-				"--disable-dev-shm-usage",
-				"--disable-features=site-per-process",
-				"--disable-gpu",
-			],
-		});
-
-		// Your Puppeteer code
-
-		await browser.close();
-	} catch (error) {
-		console.error("Error launching Puppeteer:", error);
-	}
-})();
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
