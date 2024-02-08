6b580e4ebe2bb2152eec5b2e4974424b557e7994
Antonio <esciper1@gmail.com>
2024-02-08 14:36:10 -0600

    bd changed

--- src/server/database/dbQueries.js
+++ src/server/database/dbQueries.js
@@ -3,262 +3,263 @@ const mysql = require("mysql");
 const mysql2 = require("mysql2");
 const db = require("./config");
 
-// const updateToken = (values = []) => {
-// 	try {
-// 		const pool = mysql.createPool(db);
-// 		return new Promise((resolve, reject) => {
-// 			pool.getConnection((err, connection) => {
-// 				const sql = "UPDATE usuarios SET access_token = ? WHERE id = ?";
-// 				connection.query(sql, values, (err, rows) => {
-// 					if (err) {
-// 						console.log(err);
-// 						reject(err);
-// 					}
-// 					resolve(rows);
-// 				});
-// 			});
-// 		});
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
-
-const updateToken = async (values = []) => {
+const updateToken = (values = []) => {
 	try {
-		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
+		const pool = mysql.createPool(db);
 		return new Promise((resolve, reject) => {
-			pool.query(
-				"UPDATE usuarios SET access_token = ? WHERE id = ?",
-				values,
-				(err, rows) => {
+			pool.getConnection((err, connection) => {
+				const sql = "UPDATE usuarios SET access_token = ? WHERE id = ?";
+				connection.query(sql, values, (err, rows) => {
 					if (err) {
 						console.log(err);
 						reject(err);
 					}
-					pool.end();
 					resolve(rows);
-				}
-			);
-		});
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
-};
-
-// const getUser = (values = []) => {
-// 	try {
-// 		const pool = mysql.createPool(db);
-// 		return new Promise((resolve, reject) => {
-// 			pool.getConnection((err, connection) => {
-// 				const sql =
-// 					"SELECT id, username, password FROM usuarios WHERE username = ?";
-// 				connection.query(sql, values, (err, rows) => {
-// 					if (err) {
-// 						console.log(err);
-// 						reject(err);
-// 					}
-// 					resolve(rows);
-// 				});
-// 			});
-// 		});
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
-
-const getUser = async (values = []) => {
-	try {
-		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
-		return new Promise((resolve, reject) => {
-			pool.query(
-				"SELECT id, username, password FROM usuarios WHERE username = ?",
-				values,
-				(err, rows) => {
-					if (err) {
-						console.log(err);
-						reject(err);
-					}
-					pool.end();
-					resolve(rows);
-				}
-			);
-		});
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
-};
-
-// const createCustomer = (values = []) => {
-// 	try {
-// 		const pool = mysql.createPool(db);
-// 		return new Promise((resolve, reject) => {
-// 			pool.getConnection((err, connection) => {
-// 				const sql =
-// 					"INSERT INTO clientes (firstname, lastname, phone, email) VALUES (?, ?, ?, ?)";
-// 				connection.query(sql, values, (err, rows) => {
-// 					if (err) {
-// 						console.log(err);
-// 						reject(err);
-// 					}
-// 					resolve(rows);
-// 				});
-// 			});
-// 		});
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
-
-const createCustomer = async (values = []) => {
-	try {
-		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
-		return new Promise((resolve, reject) => {
-			pool.query(
-				"INSERT INTO clientes (firstname, lastname, phone, email) VALUES (?, ?, ?, ?)",
-				values,
-				(err, rows) => {
-					if (err) {
-						console.log(err);
-						reject(err);
-					}
-					pool.end();
-					resolve(rows);
-				}
-			);
-		});
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
-};
-
-// const getCustomers = () => {
-// 	try {
-// 		const pool = mysql.createPool(db);
-// 		return new Promise((resolve, reject) => {
-// 			pool.getConnection((err, connection) => {
-// 				const sql =
-// 					"SELECT *, CONCAT(firstname, ' ', lastname) AS fullname FROM clientes";
-// 				connection.query(sql, (err, rows) => {
-// 					if (err) {
-// 						console.log(err);
-// 						reject(err);
-// 					}
-// 					resolve(rows);
-// 				});
-// 			});
-// 		});
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
-
-const getCustomers = async () => {
-	try {
-		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
-		return new Promise((resolve, reject) => {
-			pool.query(
-				"SELECT *, CONCAT(firstname, ' ', lastname) AS fullname FROM clientes",
-				(err, rows) => {
-					if (err) {
-						console.log(err);
-						reject(err);
-					}
-					pool.end();
-					resolve(rows);
-				}
-			);
-		});
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
-};
-
-// const updateCustomer = (values = []) => {
-// 	try {
-// 		const pool = mysql.createPool(db);
-// 		return new Promise((resolve, reject) => {
-// 			pool.getConnection((err, connection) => {
-// 				const sql =
-// 					"UPDATE clientes SET firstname = ?, lastname = ?, phone = ?, email = ? WHERE id = ?";
-// 				connection.query(sql, values, (err, rows) => {
-// 					if (err) {
-// 						console.log(err);
-// 						reject(err);
-// 					}
-// 					resolve(rows);
-// 				});
-// 			});
-// 		});
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
-
-const updateCustomer = async (values = []) => {
-	try {
-		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
-		return new Promise((resolve, reject) => {
-			pool.query(
-				"UPDATE clientes SET firstname = ?, lastname = ?, phone = ?, email = ? WHERE id = ?",
-				values,
-				(err, rows) => {
-					if (err) {
-						console.log(err);
-						reject(err);
-					}
-					pool.end();
-					resolve(rows);
-				}
-			);
-		});
-	} catch (error) {
-		console.error(error);
-		throw error;
-	}
-};
-
-// const deleteCustomer = (id) => {
-// 	try {
-// 		const pool = mysql.createPool(db);
-// 		return new Promise((resolve, reject) => {
-// 			pool.getConnection((err, connection) => {
-// 				const sql = "DELETE FROM clientes WHERE id = ?";
-// 				connection.query(sql, id, (err, rows) => {
-// 					if (err) {
-// 						console.log(err);
-// 						reject(err);
-// 					}
-// 					resolve(rows);
-// 				});
-// 			});
-// 		});
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// };
-
-const deleteCustomer = async (id) => {
-	try {
-		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
-		return new Promise((resolve, reject) => {
-			pool.query("DELETE FROM clientes WHERE id = ?", id, (err, rows) => {
-				if (err) {
-					console.log(err);
-					reject(err);
-				}
-				pool.end();
-				resolve(rows);
+				});
 			});
 		});
 	} catch (error) {
-		console.error(error);
-		throw error;
+		console.log(error);
 	}
 };
+
+// const updateToken = async (values = []) => {
+// 	try {
+// 		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
+// 		return new Promise((resolve, reject) => {
+// 			pool.query(
+// 				"UPDATE usuarios SET access_token = ? WHERE id = ?",
+// 				values,
+// 				(err, rows) => {
+// 					if (err) {
+// 						console.log(err);
+// 						reject(err);
+// 					}
+// 					pool.end();
+// 					resolve(rows);
+// 				}
+// 			);
+// 		});
+// 	} catch (error) {
+// 		console.error(error);
+// 		throw error;
+// 	}
+// };
+
+const getUser = (values = []) => {
+	try {
+		const pool = mysql.createPool(db);
+		console.log(pool);
+		return new Promise((resolve, reject) => {
+			pool.getConnection((err, connection) => {
+				const sql =
+					"SELECT id, username, password FROM usuarios WHERE username = ?";
+				connection.query(sql, values, (err, rows) => {
+					if (err) {
+						console.log(err);
+						reject(err);
+					}
+					resolve(rows);
+				});
+			});
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// const getUser = async (values = []) => {
+// 	try {
+// 		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
+// 		return new Promise((resolve, reject) => {
+// 			pool.query(
+// 				"SELECT id, username, password FROM usuarios WHERE username = ?",
+// 				values,
+// 				(err, rows) => {
+// 					if (err) {
+// 						console.log(err);
+// 						reject(err);
+// 					}
+// 					pool.end();
+// 					resolve(rows);
+// 				}
+// 			);
+// 		});
+// 	} catch (error) {
+// 		console.error(error);
+// 		throw error;
+// 	}
+// };
+
+const createCustomer = (values = []) => {
+	try {
+		const pool = mysql.createPool(db);
+		return new Promise((resolve, reject) => {
+			pool.getConnection((err, connection) => {
+				const sql =
+					"INSERT INTO clientes (firstname, lastname, phone, email) VALUES (?, ?, ?, ?)";
+				connection.query(sql, values, (err, rows) => {
+					if (err) {
+						console.log(err);
+						reject(err);
+					}
+					resolve(rows);
+				});
+			});
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// const createCustomer = async (values = []) => {
+// 	try {
+// 		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
+// 		return new Promise((resolve, reject) => {
+// 			pool.query(
+// 				"INSERT INTO clientes (firstname, lastname, phone, email) VALUES (?, ?, ?, ?)",
+// 				values,
+// 				(err, rows) => {
+// 					if (err) {
+// 						console.log(err);
+// 						reject(err);
+// 					}
+// 					pool.end();
+// 					resolve(rows);
+// 				}
+// 			);
+// 		});
+// 	} catch (error) {
+// 		console.error(error);
+// 		throw error;
+// 	}
+// };
+
+const getCustomers = () => {
+	try {
+		const pool = mysql.createPool(db);
+		return new Promise((resolve, reject) => {
+			pool.getConnection((err, connection) => {
+				const sql =
+					"SELECT *, CONCAT(firstname, ' ', lastname) AS fullname FROM clientes";
+				connection.query(sql, (err, rows) => {
+					if (err) {
+						console.log(err);
+						reject(err);
+					}
+					resolve(rows);
+				});
+			});
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// const getCustomers = async () => {
+// 	try {
+// 		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
+// 		return new Promise((resolve, reject) => {
+// 			pool.query(
+// 				"SELECT *, CONCAT(firstname, ' ', lastname) AS fullname FROM clientes",
+// 				(err, rows) => {
+// 					if (err) {
+// 						console.log(err);
+// 						reject(err);
+// 					}
+// 					pool.end();
+// 					resolve(rows);
+// 				}
+// 			);
+// 		});
+// 	} catch (error) {
+// 		console.error(error);
+// 		throw error;
+// 	}
+// };
+
+const updateCustomer = (values = []) => {
+	try {
+		const pool = mysql.createPool(db);
+		return new Promise((resolve, reject) => {
+			pool.getConnection((err, connection) => {
+				const sql =
+					"UPDATE clientes SET firstname = ?, lastname = ?, phone = ?, email = ? WHERE id = ?";
+				connection.query(sql, values, (err, rows) => {
+					if (err) {
+						console.log(err);
+						reject(err);
+					}
+					resolve(rows);
+				});
+			});
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// const updateCustomer = async (values = []) => {
+// 	try {
+// 		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
+// 		return new Promise((resolve, reject) => {
+// 			pool.query(
+// 				"UPDATE clientes SET firstname = ?, lastname = ?, phone = ?, email = ? WHERE id = ?",
+// 				values,
+// 				(err, rows) => {
+// 					if (err) {
+// 						console.log(err);
+// 						reject(err);
+// 					}
+// 					pool.end();
+// 					resolve(rows);
+// 				}
+// 			);
+// 		});
+// 	} catch (error) {
+// 		console.error(error);
+// 		throw error;
+// 	}
+// };
+
+const deleteCustomer = (id) => {
+	try {
+		const pool = mysql.createPool(db);
+		return new Promise((resolve, reject) => {
+			pool.getConnection((err, connection) => {
+				const sql = "DELETE FROM clientes WHERE id = ?";
+				connection.query(sql, id, (err, rows) => {
+					if (err) {
+						console.log(err);
+						reject(err);
+					}
+					resolve(rows);
+				});
+			});
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+// const deleteCustomer = async (id) => {
+// 	try {
+// 		const pool = await mysql2.createConnection(process.env.DATABASE_URL);
+// 		return new Promise((resolve, reject) => {
+// 			pool.query("DELETE FROM clientes WHERE id = ?", id, (err, rows) => {
+// 				if (err) {
+// 					console.log(err);
+// 					reject(err);
+// 				}
+// 				pool.end();
+// 				resolve(rows);
+// 			});
+// 		});
+// 	} catch (error) {
+// 		console.error(error);
+// 		throw error;
+// 	}
+// };
 
 module.exports = {
 	createCustomer,
---
+++ src/server/database/proyecto_sql.sql
@@ -0,0 +1,100 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-02-2024 a las 17:53:50
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_sql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(150) NOT NULL,
+  `lastname` varchar(150) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`id`, `firstname`, `lastname`, `phone`, `email`) VALUES
+(1, 'Jose Antonio', 'Alvarez Saucedo', '3121931090', 'esciper1@hotmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `access_token` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `password`, `access_token`) VALUES
+(1, 'admin', '$2a$12$R0nXqPC0FyB54Ycbf3I5.eDxTyiwAWyw5PHtwMTUjicBRbhmYI9u2', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiaWRVc2VyIjoxLCJpYXQiOjE3MDE4MDc5NzQsImV4cCI6MTcwMTgxMTU3NH0.0lbPJUXYx07zjvuR6MDNOoq4J3uE5ydJKThy2wtHvxs');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
