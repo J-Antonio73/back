6c8a26b38a94c3a2c3516a0c55b53ac351ac2ac5
Benito Andrade Rodriguez <73663757+bandraderdz@users.noreply.github.com>
2024-05-17 23:48:16 -0600

    Update index.js test2

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const rutas = require("./src/server/routes");
 const PORT = process.env.PORT || 3001;
-const cors = require("cors");
-
-app.use(
-	cors({
-		origin: `${process.env.CLIENT_URL}`,
-		methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-		credentials: true,
-	})
-);
-
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", `*`);
-	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-	res.setHeader(
-		"Access-Control-Allow-Headers",
-		"Content-Type, Authorization"
-	);
-	res.setHeader("Access-Control-Allow-Credentials", true);
-	next();
-});
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", rutas);
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}.`);
-	console.log(`${process.env.CLIENT_URL}`);
+  console.log(`Server is running on port ${PORT}.`);
+  console.log(`${process.env.CLIENT_URL}`);
 });
