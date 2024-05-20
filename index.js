d492d00dac14411f171548fc058838e30d5d52c2
Benito Andrade Rodriguez <73663757+bandraderdz@users.noreply.github.com>
2024-05-20 14:43:50 -0600

    Update index.js test2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app = express();
 const rutas = require("./src/server/routes");
 const PORT = process.env.PORT || 3001;
@@ -7,18 +9,22 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
+// Configuración de CORS
 app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  }));
-  
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use("/api", rutas);
-  
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-    console.log(`${process.env.CLIENT_URL}`);
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+// Middleware para parsear JSON y URL-encoded
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Rutas API
+app.use("/api", rutas);
+
+// Iniciar servidor
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
