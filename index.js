cd01d61266a6c8f14debf5a2dc421b80940210b3
Benito Andrade Rodriguez <73663757+bandraderdz@users.noreply.github.com>
2024-05-20 14:32:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const app = express();
 const rutas = require("./src/server/routes");
 const PORT = process.env.PORT || 3001;
@@ -9,17 +8,17 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use("/api", rutas);
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-  console.log(`${process.env.CLIENT_URL}`);
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }));
+  
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use("/api", rutas);
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+    console.log(`${process.env.CLIENT_URL}`);
 });
